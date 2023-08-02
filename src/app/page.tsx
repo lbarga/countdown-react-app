945999ef61c59e77da8056f6c50d08291f04e74c
Lucas Barga <lucasmpbarga@gmail.com>
2023-08-02 17:14:03 -0300

    add brazilian hour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,14 +19,13 @@ const Countdown = () => {
     if (distance <= 0) {
       setCountdownMessage("Countdown is over!");
     } else {
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
       const hours = Math.floor(
         (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
       );
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      const formattedHours = formatTimeUnit(hours);
+      const formattedHours = formatTimeUnit(hours + 3);
       const formattedMinutes = formatTimeUnit(minutes);
       const formattedSeconds = formatTimeUnit(seconds);
 
